4c8aade00185feb1cb8a284a00811d17e7cee476
Josh Mack <mack.joshuaj@gmail.com>
2021-11-29 11:49:12 -0600

    demodularized modal after addeing _redirect

--- src/App.js
+++ src/App.js
@@ -2,37 +2,74 @@ import { themeChange } from 'theme-change'
 import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import Modal from './components/Modal';
-
+import { ethers } from 'ethers';
+import threeRandomWordsNft from '../utils/ThreeRandomWordsNFT.json'
 const TWITTER_HANDLE = '_indras_net_';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-//const OPENSEA_LINK = '';
+//const OPENSEA_LINK = 'testnets.opensea.io';
 //const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x93F312De7e1C3097FA20792C3da0E1c8983D4f3a"
 
 function App() {
 
-    const [currentAccount, setCurrentAccount] = useState("");
-    
-    const checkIfWalletIsConnected = async () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+    } else {
+        console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+    } else {
+        console.log("No authorized account found")
+    }
+  }
+
+  // Method for minting the NFT
+  const askContractToMintNft = async () => {
+    // Deployed Rinkeby contract address. 
+    // Use .env environment variables to change this later if we want to switch between test networks and mainnet
+    try {
+      // We will have access to an window.Ethereum object if signed in via Metamask
       const { ethereum } = window;
 
-      if (!ethereum) {
-          console.log("Make sure you have metamask!");
-          return;
-      } else {
-          console.log("We have the ethereum object", ethereum);
-      }
+      if(ethereum) {
+        // A "Provider" helps us talk to Eth nodes
+        // See: https://docs.ethers.io/v5/api/signer/#signers
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
 
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
+        // Create the connection to our contract
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, threeRandomWordsNft.abi, signer);
+        
+        // Now we call our contract's method 
+        console.log("Going to pop wallet open to pay gas...");
+        let nftTxn = await connectedContract.makeAnNFT();
 
-      if (accounts.length !== 0) {
-          const account = accounts[0];
-          console.log("Found an authorized account:", account);
-          setCurrentAccount(account)
+        // Wait for it to be mined
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+
+        // And output the transaction hash
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`)
+
       } else {
-          console.log("No authorized account found")
-      }
+        console.log("Ethereum object doesn't exist!");
+      }        
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   /*
@@ -96,7 +133,18 @@ function App() {
           )}
           </p>
         </div>
-       <Modal/>
+        <div id="my-modal" class="modal">
+          <div class="modal-box">
+            <p>Make sure your Metamask wallet is connected to the Rinkeby test network! You can get free test Ether from a faucet;<br/> try <a href='https://faucet.rinkeby.io/' target='_blank' className='link-accent'>here</a> or <a href='https://rinkeby-faucet.com/' target='_blank' className='link-accent'> here</a>.</p> 
+            <div class="modal-action">
+              <a href="/components/modal#">
+              <button onClick={askContractToMintNft} className="btn btn-primary">
+                      Mint your NFT
+                    </button></a>
+              <a href="/components/modal#" class="btn">Close</a>
+            </div>
+          </div>
+      </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,42 +1,5 @@
-import { ethers } from 'ethers';
-import threeRandomWordsNft from '../utils/ThreeRandomWordsNFT.json'
 
-  // Method for minting the NFT
-  const askContractToMintNft = async () => {
-    // Deployed Rinkeby contract address. 
-    // Use .env environment variables to change this later if we want to switch between test networks and mainnet
-    const CONTRACT_ADDRESS = "0x93F312De7e1C3097FA20792C3da0E1c8983D4f3a"
-    try {
-      // We will have access to an window.Ethereum object if signed in via Metamask
-      const { ethereum } = window;
 
-      if(ethereum) {
-        // A "Provider" helps us talk to Eth nodes
-        // See: https://docs.ethers.io/v5/api/signer/#signers
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-
-        // Create the connection to our contract
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, threeRandomWordsNft.abi, signer);
-        
-        // Now we call our contract's method 
-        console.log("Going to pop wallet open to pay gas...");
-        let nftTxn = await connectedContract.makeAnNFT();
-
-        // Wait for it to be mined
-        console.log("Mining...please wait.");
-        await nftTxn.wait();
-
-        // And output the transaction hash
-        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`)
-
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }        
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
 const Modal = () => {
     return(
