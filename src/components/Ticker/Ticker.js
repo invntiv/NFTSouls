b4e11c9b39e077ede00fd4fc02ee8fbd464ea7b7
Josh Mack <mack.joshuaj@gmail.com>
2021-11-29 08:08:47 -0600

    compilation error

--- src/components/Ticker/Ticker.js
+++ src/components/Ticker/Ticker.js
@@ -2,7 +2,7 @@ import { themeChange } from 'theme-change'
 import React, { useEffect, useState } from "react";
 import '../../styles/App.css';
 
-export const Ticker = ({futureDate}) => {}
+export const Ticker = ({futureDate}) => {
     const now = new Date();
     const isTimeUp = isBefore(futureDate, now);
 
